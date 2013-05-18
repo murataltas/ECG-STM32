d6dda711e087d6a347c170ceccb5605b942ef784
creeep <at_1023@163.com>
2013-05-18 12:15:01 +0800

    Update test.c

--- test.c
+++ test.c
@@ -1,15 +1,1 @@
-unsigned char Buffercmp(u8* pBuffer1, u8* pBuffer2, u16 BufferLength)
-{
-  while(BufferLength--)
-  {
-    if(*pBuffer1 != *pBuffer2)
-    {
-      return 0;
-    }
-    
-    pBuffer1++;
-    pBuffer2++;
-  }
-
-  return 1;  
-}
+helloworld！
